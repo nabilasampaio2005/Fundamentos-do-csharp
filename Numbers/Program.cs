8e9eaee8d27b77cb77c6f15b282b2fba4aeaf8f2
nabilasampaio2005 <nabilasampaio@gmail.com>
2023-08-08 09:04:57 -0300

    Arquivo numbers criado com sucesso

---
+++ Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10536.25m;
+
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Ceiling(valor)); // Ceiling arredonda para cima 
+            Console.WriteLine(Math.Floor(valor)); // Floor arredonda para baixo
+            
+            
+            
+            // Formatação
+            // Console.WriteLine(
+            // valor.ToString( "C", CultureInfo.CreateSpecificCulture("pt-BR")));
+        }
+    }
+}
+
+// C mostra de acordo com a cultura especificada
+// E04 é o formato de calculadora 
+// G como se se fosse string
+// N formata para número mas sem a cultura
+// P formata com porcentagem
+// Round arredonda
